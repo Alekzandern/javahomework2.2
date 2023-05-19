bcc4689bd05f31715cf9f058e9d1a5d204ca3e7b
Alexzander <Alex.Zander54.ru>
2023-05-19 00:49:16 +0000

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        int bill = 400; // баланс рубли
+        int bill = 4000; // баланс рубли
         int bonus = (bill / 100); // количество бонусных рублей
         if (bill > 1000) {
             System.out.println("баланс:");
             System.out.println(bill + bonus); //баланс рубли итог
-        }
-        if (bill < 1000) {
+        } else {
             System.out.println("баланс:");
             System.out.println(bill); //баланс рубли итог
         }
